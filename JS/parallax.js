16c18d60f634d101d9c380e184a71f8a6ba373d3
rlskull <rafaluizsantos@gmail.com>
2026-02-28 23:44:47 -0300

    ajustes acessibilidade home ok

--- JS/parallax.js
+++ JS/parallax.js
@@ -1,31 +1,16 @@
-// Movimento Parallax da imagem
-document.addEventListener("scroll", function () {
-    const section = document.querySelector("#home05secParallax");
-    const bg = section.querySelector(".parallax-bg");
-    const rect = section.getBoundingClientRect();
+const section = document.querySelector("#home05depoimento");
 
-    const speed = 0.3; // ajuste da velocidade
-    const offset = rect.top * speed;
+if (section) {
 
-    bg.style.transform = `translateY(${offset}px)`;
-});
-
-
-// Mostrar/ocultar texto quando section entra na tela
-const section = document.querySelector("#home05secParallax");
-const texto = section.querySelector(".parallax-texto");
-
-const observer = new IntersectionObserver(
-    entries => {
+    const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                texto.style.opacity = "1";   // aparece no centro
+                section.classList.add("ativo");
             } else {
-                texto.style.opacity = "0";   // some quando a section acaba
+                section.classList.remove("ativo");
             }
         });
-    },
-    { threshold: 0.3 }  // aparece quando 30% da section está visível
-);
+    }, { threshold: 0.3 });
 
-observer.observe(section);
+    observer.observe(section);
+}
